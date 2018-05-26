1412069b653d8358758c8c25dbc696066bb3e5d8
alicialycan <alicialycan@gmail.com>
2018-05-26 14:36:54 -0700

    append cookie info to html

--- lab-alicia/backend/server.js
+++ lab-alicia/backend/server.js
@@ -1,36 +1,40 @@
 'use strict';
 
 const express = require('express');
-const dotenv = require('dotenv');
 const superagent = require('superagent');
 const app = express();
 
-dotenv.load();
+require('dotenv').config();
 
 app.get('/oauth/google/code', function(req, res) {
   if (!req.query.code) {
     res.redirect(process.env.CLIENT_URL);
   } else {
     console.log('CODE:', req.query.code);
-    superagent.post('https://www.googleleapis.com/oauth2/v4/token')
+    superagent.post('https://www.googleapis.com/oauth2/v4/token')
       .type('form')
       .send({
         code: req.query.code,
         grant_type: 'authorization_code',
         client_id: process.env.GOOGLE_CLIENT_ID,
         client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uri: `${process.env.API_URL}/`
+        redirect_uri: `${process.env.API_URL}/oauth/google/code`
       })
       .then (response => {
         console.log('Response AFTER code is given');
-        return superagent.get('https://www.googleleapis.com/plus/v1/people/me/')
-          .set('Authorization', `Bearer ${response.body.access_token}`);
-      })
-      .then(response => {
-        console.log('::::OPEN ID - Google Plus ::::', response.body);
+        console.log('access token:', response.body.access_token);
+        console.log('id token:', response.body.id_token);
+        let idTokenPayload = response.body.id_token.split('.')[1];
+        let decoded = Buffer.from(idTokenPayload, 'base64').toString();
+        let json = JSON.parse(decoded);
+        console.log('decoded:', decoded);
         // handle oauth login
-        res.cookie('X-Some Cookie', 'some token');
-        res.redirect(process.env.CLIENT_URL);
+        res.cookie('X-Some Cookie', idTokenPayload);
+        res.write('<h1>' + json.email + '</h1>');
+        res.end();
+      })
+      .catch(response => {
+        console.log('response', response);
       });
   }
 });
