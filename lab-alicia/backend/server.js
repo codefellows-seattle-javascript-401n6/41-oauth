b136f8fc4520b292c3893893cf2a8e7eddfc9c25
alicialycan <alicialycan@gmail.com>
2018-05-26 13:42:59 -0700

    add client info

--- lab-alicia/backend/server.js
+++ lab-alicia/backend/server.js
@@ -1,54 +1,44 @@
 'use strict';
 
 const express = require('express');
-const superagent = require('superagent');
 const dotenv = require('dotenv');
+const superagent = require('superagent');
 const app = express();
 
 dotenv.load();
 
-app.get('/', (req, res) => {
-  res.sendFile('./index.html', {root: '/'});
-});
-
-app.get('oauth-callback', (req, res) => {
-  
-  let code = req.query.code;
-  let state = req.query.state;
-
-  let url = 'https://github.com/login/oauth/access_token';
-  superagent.post(url)
-    .send({
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      redirect_uri: process.env.REDIRECT_URI,
-      code: code,
-      state:  state,
-      scopes: 'repo',
-    })
-    .then((response) => {
-      let token = response.body.access_token;
-      let userUrl = 'https://api.github.com/user?';
-      userUrl += 'access_token=' + token;
-      return superagent.get(userUrl);
-    })
-    .then(response => {
-
-    })
-
-});
-
-// need a way to detect if someone's logged in or not
-app.get('/profile', (req, res) => {
-  let needle = 'oauth-token';
-  let isLoggedIn = req.headers.cookie.includes(needle);
-  if (!isLoggedIn) {
-    res.write('<h1>Unauthorized</h1>');
-    res.write('<p>You must login to view this page</p>');
+app.get('/oauth/google/code', function(req, res) {
+  if (!req.query.code) {
+    res.redirect(process.env.CLIENT_URL);
+  } else {
+    console.log('CODE:', req.query.code);
+    superagent.post('https://www.googleleapis.com/oauth2/v4/token')
+      .type('form')
+      .send({
+        code: req.query.code,
+        grant_type: 'authorization_code',
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_uri: `${process.env.API_URL}/`
+      })
+      .then (response => {
+        console.log('Response AFTER code is given');
+        return superagent.get('https://www.googleleapis.com/plus/v1/people/me/')
+          .set('Authorization', `Bearer ${response.body.access_token}`);
+      })
+      .then(response => {
+        console.log('::::OPEN ID - Google Plus ::::', response.body);
+        // handle oauth login
+        res.cookie('X-Some Cookie', 'some token');
+        res.redirect(process.env.CLIENT_URL);
+      });
   }
 });
 
-let PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('http://localhost:3000');
+app.get('/auth-test', (req, res) => {
+  res.sendFile('./index.html', {root: './'});
+});
+
+app.listen(3000, () => {
+  console.log('listen on port 3000');
 });
