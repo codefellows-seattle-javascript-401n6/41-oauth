a6af90eb54cf088426c9718a91d1ca6f85dd75a4
DANIEL PILLAY <DPILLAY03@GMAIL.COM>
2018-05-29 19:59:50 -0700

    link is directing to github

--- lab-daniel/index.js
+++ lab-daniel/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', {root: './'});
+})
+
+app.get('/github-login', (req, res) => {
+    res.send(req)
+})
 app.listen(PORT, () => {
     console.log(`listening on http://localhost${PORT}`)
 });
