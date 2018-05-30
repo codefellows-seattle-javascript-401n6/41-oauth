4e0e0c294f0b71cdacf1c99c678062b278b56d43
DANIEL PILLAY <DPILLAY03@GMAIL.COM>
2018-05-29 22:02:42 -0700

    successfully loggin into github. working on cookies

--- lab-daniel/index.js
+++ lab-daniel/index.js
@@ -18,10 +18,25 @@ app.get('/oauth-callback', (req, res) => {
         client_id: process.env.CLIENT_ID,
         client_secret: process.env.CLIENT_SECRET,
         code: code
-    }).then(tokenResponse => {
-        res.send(tokenResponse.body);
-        let token = tokenResponse.body.token;
-    });
+    })
+    .then(tokenResponse => {
+        let token = tokenResponse.body.access_token;
+        let userUrl = 'https://api.github.com/user?';
+        userUrl += 'access_token=' + token;
+        return superagent.get(userUrl);
+    }).then(userResponse => {
+        let userName = userResponse.body.login;
+        let bio = userResponse.body.bio;
+        res.write('<a href="http://localhost:3000"> << home </a>')
+        res.write('<img src="' + userResponse.body.avatar_url + '"/>')
+        res.write('<h3>' + userName + '</h3>')
+        res.write('<p>' + bio + '</p>')
+        res.write('<pre' + JSON.stringify(userResponse.body) + '</pre>')
+        res.end()
+    })
+    .catch(err => {
+        res.send(err.body.message);
+    })
 });
 app.listen(PORT, () => {
     console.log(`listening on http://localhost${PORT}`)
