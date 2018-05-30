080d4eb86d1896f15b9f4a37ff2faf7bd518a8c6
cody <codyjeromegreen@gmail.com>
2018-05-29 21:42:43 -0700

    continuing lab 41

---
+++ lab-cody/index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.load();
+
+app.get('/oauth-callback', function(req, res) {
+  console.log('callback', req.query.code);
+
+  let code = req.query.code;
+  let state = req.query.state;
+
+  let url = 'https://github.com/login/oauth/access_token';
+  superagent
+    .post(url)
+    .send({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      redirect_uri: process.env.REDIRECT_URI,
+      code: code,
+      state: state,
+      scopes: 'repo'
+    })
+    .then(response => {
+      console.log('token', response.body);
+
+      let user = 'codyjgreen';
+      let userUrl = 'https://api.github.com/user';
+      console.log('getting resources from:', userUrl);
+      return superagent
+        .get(userUrl)
+        .set('Authorization', 'token ' + response.body.access_token);
+    })
+    .then(response => {
+      console.log('access response', response);
+      res.send(response.body);
+      res.end();
+    })
+    .catch(err => {
+      console.log(err.message);
+      res.send(err.message);
+    });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', { root: './' });
+});
+
+app.get('/cookie-setter', (req, res) => {
+  res.cookie('my-custom-cookie', 'snickerdoodle', { maxAge: 900000 });
+  res.cookie('my-short-lived-cookie', 'ten seconds', { maxAge: 10000 });
+  res.cookie('my-longer-lived-cookie', 'one-hundred seconds!', {
+    maxAge: 100000
+  });
+  res.send('<h1>setting cookies</h1>');
+});
+
+app.get('/cookie-inspector', (req, res) => {
+  console.log('cookie req', req);
+  res.write('<h1>reading cookies</h1>');
+  res.write('<pre>' + req.headers.cookie + '</pre>');
+  res.end();
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
