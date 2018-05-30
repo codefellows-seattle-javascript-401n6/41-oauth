fee88ed5ea75981a683ee77d23acc922d9c9a261
DANIEL PILLAY <DPILLAY03@GMAIL.COM>
2018-05-29 20:42:47 -0700

    access token is being printed

--- lab-daniel/index.js
+++ lab-daniel/index.js
@@ -1,14 +1,28 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
+let superagent = require('superagent')
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile('./index.html', {root: './'});
 })
 
-app.get('/github-oauth', (req, res) => {
-    res.send(req)
-})
+app.get('/oauth-callback', (req, res) => {
+    let {code, state} = req.query;
+
+    let tokenUrl = 'https://github.com/login/oauth/access_token'
+    superagent.post(tokenUrl)
+    .send({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code: code
+    }).then(tokenResponse => {
+        res.send(tokenResponse.body);
+        let token = tokenResponse.body.token;
+    });
+});
 app.listen(PORT, () => {
     console.log(`listening on http://localhost${PORT}`)
 });
